fcea00055f69e533814852a254aa9738aa67a361
Benedikt Schnatterbeck <benediktms@hey.com>
2022-06-04 10:34:10 +0100

    Add mouse input detection

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    bool isPlaceable;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        if (isPlaceable)
+        {
+
+            Debug.Log(transform.name);
+        }
     }
 }
