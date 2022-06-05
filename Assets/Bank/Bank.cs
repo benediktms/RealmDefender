50577382cebe4a9cf285f6b1255fcb96e46abb3b
Benedikt Schnatterbeck <benediktms@hey.com>
2022-06-05 09:48:48 +0100

    Reload scene when reaching negative balance

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
@@ -24,5 +25,15 @@ public class Bank : MonoBehaviour
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+
+        if (currentBalance < 0)
+        {
+            ReloadScene();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
